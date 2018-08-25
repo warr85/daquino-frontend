4b08c428c6f9ec428f51fe931dadbe6f01f77e2d
Wilmer Ramones <wilmer.ramones@gmail.com>
2018-08-25 09:58:54 -0400

    evita que el usuario ingrese a la app si no está logueado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { UserService } from './../../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ UserService ]
 })
 export class HomeComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(
+    private _userService: UserService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this.isLogged();
+  }
+
+  isLogged() {
+      let identity = this._userService.getIdentity();
+      if(identity === null || !identity.sub){
+        this._router.navigate(["/login"]);
+     }
   }
 
 }
